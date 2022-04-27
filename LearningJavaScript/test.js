d7ebc7012efffeda09a6d1635b457ec5f95af3dc
Prince Bansal <prince.bansal008@gmail.com>
2022-04-28 02:01:35 +0530

    test file added to varify rules

---
+++ LearningJavaScript/test.js
@@ -0,0 +1,16 @@
+const val = { name: "prince" };
+console.log(val);
+
+val.name = "bansal";
+console.log(val);
+
+// val = 69;                 ERROR
+// val = { key: "value" };   ERROR
+// console.log(val);
+
+console.log(x);
+var x = 69;
+console.log(x);
+
+var x = "Prince";
+console.log(x);
