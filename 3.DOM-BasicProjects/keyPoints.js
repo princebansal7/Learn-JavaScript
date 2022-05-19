b9fef8b4225479cf69fe9f1ba077996e94e89768
Prince Bansal <prince.bansal008@gmail.com>
2022-05-20 04:20:51 +0530

    file added & updated

---
+++ 3.DOM-BasicProjects/keyPoints.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// Getting HTML elements using JS
+
+// document.querySelector(); // gives the whole element which have the class passed in argument
+document.querySelector('.message'); // for class
+// document.querySelector('#message'); // for id
+
+console.log(document.querySelector('.message'));
